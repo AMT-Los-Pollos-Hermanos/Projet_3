fa4287310281c2744f15af29582eb7860969df06
Julien Béguin <julien.beguin99@gmail.com>
2021-01-08 20:27:26 +0100

    add file's header comment

--- src/main/java/ch/heig/amt/overflow/application/gamification/PointScaleDTO.java
+++ src/main/java/ch/heig/amt/overflow/application/gamification/PointScaleDTO.java
@@ -1,3 +1,9 @@
+/*
+ * AMT : Project 3 - Overflow
+ * Authors : Gil Balsiger, Chris Barros Henriques, Julien Béguin & Gaëtan Daubresse
+ * Date : 08.01.2021
+ */
+
 package ch.heig.amt.overflow.application.gamification;
 
 import lombok.Builder;
